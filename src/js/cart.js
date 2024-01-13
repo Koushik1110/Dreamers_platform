661d41efd26b8d62b03f4dfb4c6097147f6ff7a1
Koushik1110 <148125270+Koushik1110@users.noreply.github.com>
2024-01-14 02:55:08 +0600

    single product added

--- src/js/cart.js
+++ src/js/cart.js
@@ -1,11 +1,18 @@
+import fetchSingleProduct from "./single-product";
+
 const productsWrapper = document.querySelector(".products_wrapper");
 
 export default function addToCart() {
   productsWrapper.addEventListener("click", (e) => detectProduct(e));
 }
 
-function detectProduct(e) {
+async function detectProduct(e) {
   if (e.target.classlist.contains("add_to_cart_btn")) {
     const btn = e.target;
+    const productId = +btn.dataset.id;
+
+    // fetch single product based on id
+    const product = await fetchSingleProduct(productId);
+    console.log(product);
   }
 }
