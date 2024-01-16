63d89439b955c63784653cf887b6a5b176fb006f
Koushik1110 <148125270+Koushik1110@users.noreply.github.com>
2024-01-16 13:55:49 +0600

    single product complete

--- src/js/single-product.js
+++ src/js/single-product.js
@@ -1,10 +1,15 @@
-export default async function fetchSingleProduct(id) {
+import { renderSingleProductError, renderSingleProductLoading } from "./render";
+
+export default async function fetchSingleProduct(id, btn) {
   try {
-    const res = await fetch(`https://fakestoreapi.com/products/${id}`);
-    if (!res.ok) throw new Error("product not found");
+    renderSingleProductLoading(true, btn);
+    const res = await fetch(`https://dummyjson.com/products/${id}`);
+    if (!res.ok) throw new Error("Product not found");
     const data = await res.json();
     return data;
   } catch (error) {
-    console.log(error.message);
+    renderSingleProductError(error.message);
+  } finally {
+    renderSingleProductLoading(false, btn);
   }
 }
